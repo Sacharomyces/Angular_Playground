f2a310407eefeb2520dfc072d430d61aeee929ef
Sacharomyces <johndoe@example.cob.mackiewicz1@gmail.com>
2017-04-27 13:44:52 +0200

    bindings tested

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,19 @@ import { BookService } from "app/book.service";
   <h1>{{title}}</h1>
   <products></products>
   <books></books>
+  <img src={{imageUrl}}/>
+  <button 
+        class="btn btn-primary"
+        [class.active]="isActive">Naciśnij</button>
+ 
+  <button 
+        class="btn btn-primary"
+        [style.backgroundColor]="isActive ? 'blue':'red'">Naciśnij</button>
+
+   <button (click)="onClick($event)">kliknij</button>
+   <input type="text" [(ngModel)]="typedText"/>
+   Napisałeś:{{typedText}}
+  
   `,
   providers:[
     ProductService,
@@ -17,4 +30,11 @@ import { BookService } from "app/book.service";
 })
 export class AppComponent {
   title = 'Moja apka';
+  imageUrl="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRwiKw9xPE6ZTl490JW-wqvQZD5shpSraY8CLCs5-8VEMK7sO1H"
+  isActive=true;
+  onClick($event){
+    this.isActive= !this.isActive;
+    console.log("clicked");
+ var typedText =""; 
+  }
 }
